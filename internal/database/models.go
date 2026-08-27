4269377c3a41ab0b75b0fc6407aae556e4af4a37
wasiq345 <wasiqazeem000@gmail.com>
2026-08-27 17:35:55 +0500

    Contains CreateRefreshToken and UpdateRefreshToken go code

--- internal/database/models.go
+++ internal/database/models.go
@@ -5,6 +5,7 @@
 package database
 
 import (
+	"database/sql"
 	"time"
 
 	"github.com/google/uuid"
@@ -15,6 +16,15 @@ type Note struct {
 	DailyNote string
 	CreatedAt time.Time
 	UpdatedAt time.Time
+}
+
+type RefreshToken struct {
+	Token     string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	ExpiresAt time.Time
+	RevokedAt sql.NullTime
+	Userid    uuid.UUID
 }
 
 type User struct {
