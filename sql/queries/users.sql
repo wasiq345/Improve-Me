3d0f193b00ac62c623f3216b1968edf183f21f84
wasiq345 <wasiqazeem000@gmail.com>
2026-08-29 23:32:33 +0500

    fixed sql query

--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -1,6 +1,6 @@
 -- name: RegisterUser :one
 INSERT INTO Users (id, created_at, email, password_hash) values (
-    get_random_uuid(), NOW(), $1, $2
+    gen_random_uuid(), NOW(), $1, $2
 )
 RETURNING *;
 
