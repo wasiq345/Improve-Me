145a2fe71e6c5f35ee8ccf35a672098235b0437b
wasiq345 <wasiqazeem000@gmail.com>
2026-09-03 01:14:08 +0500

    modified the port number

--- internal/api/middleware.go
+++ internal/api/middleware.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func EnableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://127.0.0.1:5500")
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
