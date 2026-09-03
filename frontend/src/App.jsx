bcb2e54e805074697da8c68ecf966f3f0d6b820f
wasiq345 <wasiqazeem000@gmail.com>
2026-09-03 18:53:29 +0500

    Added diff paths using react router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,18 @@
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import React from 'react'
+import Dashboard from './dashboard'
 import Login from './login'
+import Register from './register'
 
 export default function App() {
   return (
-    <div>
-      <Login />
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Dashboard />} />
+        <Route path='/LoginUser' element={<Login />} />
+        <Route path='/RegisterUser' element={<Register />} />
+      </Routes>
+    </Router>
   )
 }
 
