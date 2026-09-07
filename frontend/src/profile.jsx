9ae813b5cffae7efeb683bf7b2ac65b0a57cd9b6
wasiq345 <wasiqazeem000@gmail.com>
2026-09-07 23:18:33 +0500

    did bit of work

--- frontend/src/profile.jsx
+++ frontend/src/profile.jsx
@@ -1,6 +1,23 @@
 import React from "react"
 
 export default function Profile() {
+    const fetchProfileData = async (e) => {
+        const username = "xyz"
+        const token = localStorage.getItem("accessToken")
+        e.preventDefault();
+            const response = await fetch(`http://localhost:8080/Profile?username={username}`, {
+                method: 'GET',
+                headers: {
+                     'Authorization': 'Bearer ${token}',
+                     'Content-Type': 'application/json'
+                }
+            });
+            if(response.ok) {
+                const data = await response.json();
+                // 
+            } else console.error("failed to fetch")
+        }
+
     return (
         <div>
         <h1>Welcome to Your Profile</h1>
